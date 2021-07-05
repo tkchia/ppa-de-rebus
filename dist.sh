664eb8e15ea65158615a1940f3fd01876c8cf9ce
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2021-07-05 15:53:43 +0000

    Allow emu2 to be built for multiple Ubuntu distributions

--- dist.sh
+++ dist.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # Create Debian source package.  Public domain.
 
+set -e
 case "$DEBSIGN_KEYID" in
     '')	signing=("-us" "-uc");;
     *)	signing=("-k$DEBSIGN_KEYID");;
 esac
-exec debuild --no-tgz-check -i -S -rfakeroot -d ${signing[@]}
+if test 0 = $#; then
+	exec debuild --no-tgz-check -i -S -rfakeroot -d ${signing[@]}
+else
+	if test \! -f debian/changelog.in; then
+		echo 'no debian/changelog.in!' >&2
+		exit 1
+	fi
+	trap 'rm -f debian/changelog' ERR EXIT TERM QUIT
+	for distro in "$@"; do
+		sed "s/@distro@/$distro/g" debian/changelog.in \
+		    >debian/changelog
+		debuild --no-tgz-check -i -S -rfakeroot -d ${signing[@]}
+	done
+fi
